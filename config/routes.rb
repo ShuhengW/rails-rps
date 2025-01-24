2e9ca2c662b814e748e08e2d8f72f0639a3b2074
ShuhengW <wsh9487@gmail.com>
2025-01-24 16:12:58 -0600

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
 
+get("/rock", { :controller => "rbs", :action => "playrock" })
+get("/paper", { :controller => "rbs", :action => "playpaper" })
+get("/scissors", { :controller => "rbs", :action => "playscissors" })
+
 end
