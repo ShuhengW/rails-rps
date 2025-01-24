6e47d9b54bb08dfd181dc4fc18ee6e874a635746
Steven Wang <34365342+ShuhengW@users.noreply.github.com>
2025-01-24 22:17:42 +0000

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
-get("/rock", { :controller => "rbs", :action => "playrock" })
-get("/paper", { :controller => "rbs", :action => "playpaper" })
-get("/scissors", { :controller => "rbs", :action => "playscissors" })
+get("/rock", { :controller => "rbs_controller", :action => "playrock" })
+get("/paper", { :controller => "rbs_controller", :action => "playpaper" })
+get("/scissors", { :controller => "rbs_controller", :action => "playscissors" })
 
 end
